a34659d6bd756f0a54557f4de061a4ed5c3e4bd5
JarrColl <15962073+JarrColl@users.noreply.github.com>
2026-04-25 20:10:32 +1000

    fix: (fish) deprioritise homebrew binaries below system binaries. (#21)

--- system_files/usr/share/fish/vendor_conf.d/ublue-brew.fish
+++ system_files/usr/share/fish/vendor_conf.d/ublue-brew.fish
@@ -1,13 +1,17 @@
 #!/usr/bin/fish
 #shellcheck disable=all
 if status --is-interactive
-    if [ -d /home/linuxbrew/.linuxbrew ]
-        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
+    if test -d /home/linuxbrew/.linuxbrew
+        /home/linuxbrew/.linuxbrew/bin/brew shellenv fish | source
+
+        # Prioritize system binaries to prevent brew overriding things like dbus
+        fish_add_path --move --append --path (brew --prefix)/bin (brew --prefix)/sbin
+
         if test -d (brew --prefix)/share/fish/completions
-            set -p fish_complete_path (brew --prefix)/share/fish/completions
+            set -ga fish_complete_path (brew --prefix)/share/fish/completions
         end
         if test -d (brew --prefix)/share/fish/vendor_completions.d
-            set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+            set -ga fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
         end
     end
 end
